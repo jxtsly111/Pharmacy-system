c876c9507565e01fb9071e4c10533eb66661f5cf
Your Name <jxtsly@gmail.com>
2023-10-19 11:37:26 +0200

    done with login logics

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,4 +9,17 @@ class LoginController extends Controller
     public function login(){
         return view("welcome");
     }
+
+    public function authenticate(Request $request)
+{
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        // Authentication passed
+        return redirect('/dashboard'); // Redirect to a dashboard or home page
+    } else {
+        // Authentication failed
+        return redirect()->route('login')->with('error', 'Invalid login credentials');
+    }
+}
 }
