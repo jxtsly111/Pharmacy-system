08b36f28db34126d5a92d04214cf147fbe4b4942
Your Name <jxtsly@gmail.com>
2023-10-21 10:27:48 +0200

    completion of product ui

---
+++ resources/views/includes/link.blade.php
@@ -0,0 +1,14 @@
+<link rel="shortcut icon" type="image/x-icon" href="{{asset('style/img/favicon.png')}}">
+
+<link rel="stylesheet" href="{{asset('style/css/bootstrap.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('style/css/animate.css')}}">
+
+<link rel="stylesheet" href="{{asset('style/plugins/select2/css/select2.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('style/css/dataTables.bootstrap4.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('style/plugins/fontawesome/css/fontawesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('style/plugins/fontawesome/css/all.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('style/css/style.css')}}">
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -14,8 +14,11 @@
     @include('includes.navbar')
     <!-- End Navbar -->
     <div class="card shadow-lg mx-4 card-profile-bottom">
-
-
+      <div class="card-body p-3">
+        <div class="row gx-4">
+          <div class="col-auto">
+            
+          </div>
 
           @if (session('success'))
           <div class="alert alert-success">
@@ -33,8 +36,103 @@
               </ul>
           </div>
       @endif
-
-
+          <div class="col-auto my-auto">
+            <div class="h-100">
+              <h5 class="mb-1">
+                Product Management
+              </h5>
+              <p class="mb-0 font-weight-bold text-sm">
+                Adding product information is the process of gathering, storing, and maintaining essential details about products in a healthcare system.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
+            <div class="nav-wrapper position-relative end-0">
+              <ul class="nav nav-pills nav-fill p-1" role="tablist">
+                <li class="nav-item">
+                  <a href="{{route('information')}}" class="nav-link mb-0 px-0 py-1 active d-flex align-items-center justify-content-center " >View All Product Informations</a>
+                </li>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid py-4">
+      <div class="row">
+        <div class="col-md-12">
+          <form class="card" method="POST" action="{{route('addPatient')}}">@csrf
+            <div class="card-header pb-0">
+              <div class="d-flex align-items-center">
+                <button class="btn btn-primary btn-sm ms-auto" type="submit">Add Product</button>
+              </div>
+            </div>
+            <div class="card-body">
+              <p class="text-uppercase text-sm">Product Information:</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Product Name</label>
+                    <input class="form-control" type="text" name="product_name" >
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Quantity</label>
+                    <input class="form-control" type="text" name="Quantity" >
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Tax(percentage)</label>
+                    <input class="form-control" type="text" name="tax" >
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Discount(percentage)</label>
+                    <input class="form-control" type="text" name="discount">
+                  </div>
+                </div>
+              </div>
+              <hr class="horizontal dark">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Price</label>
+                    <input class="form-control" type="text"  name="price">
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Status</label>
+                    <input class="form-control" type="text" name="status">
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Product Image</label>
+                    <input class="form-control" type="file" name="image" >
+                  </div>
+                </div>
+              </div>
+              <hr class="horizontal dark">
+              <p class="text-uppercase text-sm">Description</p>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Description</label>
+                    <input class="form-control" type="text" name="description" >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+       
+      </div>
+      @include('includes.footer')
     </div>
   </div>
   @include('includes.plugins')
