058fbd086d704b223148b9c4f907977d62c7c97c
Your Name <jxtsly@gmail.com>
2023-10-22 10:18:42 +0200

    done with product model logics

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_name',
+        'quantity',
+        'tax',
+        'discount',
+        'price',
+        'status',
+        'image',
+        'description',
+    ];
 }
